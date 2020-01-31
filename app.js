eceda85997161f2f005ad367508656d0627add12
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-01-31 23:10:07 +0530

    View engine setup complete

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+
 const app = express();
 
+app.engine('handlebars', exphbs()); // Initializing the handlebars view engine
+app.set('view engine', 'handlebars'); // Setting the view engine to handlebars
+
 app.get('/', (req, res) => {
-    res.send("check");
-})
+    res.render('home');
+});
+
+
 
 const PORT = 3000;
 app.listen(`${PORT}`);
