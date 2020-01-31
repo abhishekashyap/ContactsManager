16923d9a5b4058b9d7b74f5209dfb8c72b42ceeb
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-02-01 00:24:13 +0530

    Added basic styling

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const path = require('path');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', exphbs()); // Initializing the handlebars view engine
 app.set('view engine', 'handlebars'); // Setting the view engine to handlebars
