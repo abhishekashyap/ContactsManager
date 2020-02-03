1b75e96749077c3174750be96f058d53237ae769
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-02-03 10:46:55 +0530

    Added burger menu functionality

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
     module.exports = {
-        mongoURI: '<Your db string here>'
+        mongoURI: 'mongodb+srv://Abhishek:abhishek@cluster0-by1gm.mongodb.net/test?retryWrites=true&w=majority'
     }
 } else {
     module.exports = {
