e88ff84087738d1c4f3c1b22028d2db20ad1bd58
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-02-03 10:16:05 +0530

    Production build

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
     module.exports = {
-        mongoURI: 'mongodb+srv://Abhishek:abhishek@cluster0-by1gm.mongodb.net/test?retryWrites=true&w=majority'
+        mongoURI: ''
     }
 } else {
     module.exports = {
